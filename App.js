eb0964079f56f2abc8281a6f983d9e2074c26be1
truongvlit <truongvlit@gmail.com>
2020-09-19 03:21:52 +0700

    setup for i18n

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+// Set the key-value pairs for the different languages you want to support.
+i18n.translations = {
+  en: { welcome: 'Hello', name: 'Charlie' },
+  ja: { welcome: 'こんにちは' },
+};
+// Set the locale once at the beginning of your app.
+i18n.locale = Localization.locale;
+// i18n.locale = "ja-jp";
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.fallbacks = true;
+
+// console.log(i18n.locale);
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
+    <Text category='h1'>{i18n.t('welcome')} {i18n.t('name')}</Text>
   </Layout>
 );
 
