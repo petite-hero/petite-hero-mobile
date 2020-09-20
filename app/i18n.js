461504a2a4a8805b3b8aeddb6afca195e9bb8b0b
truongvlit <truongvlit@gmail.com>
2020-09-20 17:44:58 +0700

    setup for i18n

---
+++ app/i18n.js
@@ -0,0 +1,25 @@
+import viTranslationMessages from './translations/vi.json';
+import enTranslationMessages from './translations/en.json';
+
+const DEFAULT_LOCALE = 'en';
+
+const formatTranslationMessages = (locale, messages) => {
+  const defaultFormattedMessages =
+    locale !== DEFAULT_LOCALE
+      ? formatTranslationMessages(DEFAULT_LOCALE, viTranslationMessages)
+      : {};
+  return Object.keys(messages).reduce((formattedMessages, key) => {
+    let message = messages[key];
+    if (!message && locale !== DEFAULT_LOCALE) {
+      message = defaultFormattedMessages[key];
+    }
+    return Object.assign(formattedMessages, {[key]: message});
+  }, {});
+};
+
+const translationMessages = {
+  vi: formatTranslationMessages('vi', viTranslationMessages),
+  en: formatTranslationMessages('en', enTranslationMessages),
+};
+
+export { translationMessages };
