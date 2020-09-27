2f3ec3959d1540eb28d6b390598e01b605d71483
hulk1999 <hungng300899@gmail.com>
2020-09-27 22:47:50 +0700

    initilize maps and search bar

--- app/main/components/LoginScreen/index.js
+++ app/main/components/LoginScreen/index.js
@@ -1,16 +1,61 @@
 import React, { useContext } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Keyboard, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Icon } from 'react-native-elements'
 import styles from './styles/index.css';
 
 const LoginScreen = (props) => {
-  const { t } = useContext(props.route.params.locale);
+
+  [location, setLocation] = React.useState({latitude: 10.8413525, longitude: 106.8108569});  // FPT University location
+  [searchBarFocused, setSearchBarFocused] = React.useState(false);
+  [searchBar, setSearchBar] = React.useState(null);
+
+  navigator.geolocation.getCurrentPosition(
+      (data) => {
+        setLatitude(data.coords.latitude);
+        this.setState({longitude: data.coords.longitude});
+      }
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>This is TrackingScreen</Text>
-        <Text>{t('welcome-test') + ", " + t('hero-test')}</Text>
-      </View>
+        <View style={{...StyleSheet.absoluteFillObject}}>
+          <MapView
+            style={{width: Dimensions.get('window').width, height: Dimensions.get('window').height}}
+            region={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}}
+            showsUserLocation={true}
+            showsMyLocationButton={false}>
+            <Marker coordinate={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}} />
+          </MapView>
+        </View>
+        <View style={searchBarFocused ? styles.searchBarContainerFocused : styles.searchBarContainer}>
+          <GooglePlacesAutocomplete
+            ref={(instance) => { setSearchBar(instance) }}
+            styles={searchBarFocused ?
+              {textInputContainer: styles.textInputContainerFocused, textInput: styles.textInput, listView: styles.listView} :
+              {textInputContainer: styles.textInputContainer, textInput: styles.textInput, listView: styles.listView}}
+            placeholder='Search'
+            fetchDetails={true}
+            onPress={(data, details = null) => {
+              setLocation({latitude: details.geometry.location.lat, longitude: details.geometry.location.lng});
+              setSearchBarFocused(false);
+              searchBar.setAddressText("");
+            }}
+            textInputProps={{
+              onFocus: () => setSearchBarFocused(true),
+            }}
+            query={{key: 'AIzaSyBvfVumttk96MLwUy-oLqaz3OqtGSIAejk', components: 'country:vn',}}
+            debounce={150}
+          />
+        </View>
+        <View style={searchBarFocused ? styles.searchBackBtnFocused : styles.searchBackBtn}>
+          <Icon name='arrow-left' type='font-awesome'
+            onPress={() => {
+              setSearchBarFocused(false);
+              Keyboard.dismiss();
+            }}/>
+        </View>
     </SafeAreaView>
   );
 };
--- app/main/components/LoginScreen/styles/index.css.js
+++ app/main/components/LoginScreen/styles/index.css.js
@@ -10,6 +10,67 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     paddingLeft: wp('10%'),
     paddingRight: wp('10%'),
+  },
+  searchBarContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: "auto",
+    left: 0,
+    right: 0,
+    height: 74,
+    backgroundColor: 'rgba(0, 0, 0, 0)'
+  },
+  searchBarContainerFocused:{
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "auto",
+    backgroundColor: 'white'
+  },
+  textInputContainer: {
+    marginLeft: 50,
+    marginRight: 0,
+    marginTop: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    height: 74,
+    borderTopWidth: 0,
+    borderBottomWidth: 0
+  },
+  textInputContainerFocused: {
+    marginLeft: 50,
+    marginRight: 0,
+    marginTop: 0,
+    backgroundColor: 'white',
+    height: 74,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+  },
+  textInput: {
+    marginLeft: 0,
+    marginRight: 10,
+    marginTop: 30,
+    marginBottom: 0,
+    paddingLeft: 15,
+    borderWidth: 1,
+    borderColor: 'rgb(200, 200, 200)',
+    borderRadius: 20,
+    height: 44,
+    fontSize: 18,
+  },
+  listView: {
+    backgroundColor: 'white'
+  },
+  searchBackBtn: {
+    display: "none"
+  },
+  searchBackBtnFocused: {
+    position: "absolute",
+    top: 40,
+    left: 10,
+    height: 25,
+    width: 25
   }
 });
 
