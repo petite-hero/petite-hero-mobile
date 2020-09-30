8b4a5a7471a124ed9c92cf1c64bf6fcc343de523
hulk1999 <hungng300899@gmail.com>
2020-09-30 21:13:52 +0700

    update control panel

--- app/main/components/LoginScreen/index.js
+++ app/main/components/LoginScreen/index.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { SafeAreaView, View, StyleSheet, Keyboard, Dimensions } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, StyleSheet, Keyboard, Dimensions, Image, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { Icon } from 'react-native-elements'
@@ -20,42 +20,59 @@ const LoginScreen = (props) => {
 
   return (
     <SafeAreaView style={styles.container}>
-        <View style={{...StyleSheet.absoluteFillObject}}>
-          <MapView
-            style={{width: Dimensions.get('window').width, height: Dimensions.get('window').height}}
-            region={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}}
-            showsUserLocation={true}
-            showsMyLocationButton={false}>
-            <Marker coordinate={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}} />
-          </MapView>
-        </View>
-        <View style={searchBarFocused ? styles.searchBarContainerFocused : styles.searchBarContainer}>
-          <GooglePlacesAutocomplete
-            ref={(instance) => { setSearchBar(instance) }}
-            styles={searchBarFocused ?
-              {textInputContainer: styles.textInputContainerFocused, textInput: styles.textInput, listView: styles.listView} :
-              {textInputContainer: styles.textInputContainer, textInput: styles.textInput, listView: styles.listView}}
-            placeholder='Search'
-            fetchDetails={true}
-            onPress={(data, details = null) => {
-              setLocation({latitude: details.geometry.location.lat, longitude: details.geometry.location.lng});
-              setSearchBarFocused(false);
-              searchBar.setAddressText("");
-            }}
-            textInputProps={{
-              onFocus: () => setSearchBarFocused(true),
-            }}
-            query={{key: 'AIzaSyBvfVumttk96MLwUy-oLqaz3OqtGSIAejk', components: 'country:vn',}}
-            debounce={150}
-          />
-        </View>
-        <View style={searchBarFocused ? styles.searchBackBtnFocused : styles.searchBackBtn}>
-          <Icon name='arrow-left' type='font-awesome'
-            onPress={() => {
-              setSearchBarFocused(false);
-              Keyboard.dismiss();
-            }}/>
-        </View>
+
+      {/* child img */}
+      <View style={styles.header}>
+        <Image
+          style={styles.avatar}
+          source={{uri: "https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.15752-9/118881393_430697914571214_4949863648741553269_n.jpg?_nc_cat=107&_nc_sid=ae9488&_nc_ohc=CRL20t0CXSoAX-UGsNg&_nc_ht=scontent.fsgn2-3.fna&oh=8a78db6a5556a3e8d4039464250d0c91&oe=5F91B50E"}}
+        />
+      </View>
+
+      {/* maps */}
+      <View style={{...StyleSheet.absoluteFillObject}}>
+        <MapView
+          style={{width: Dimensions.get('window').width, height: Dimensions.get('window').height}}
+          region={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}}
+          showsUserLocation={true}
+          showsMyLocationButton={false}>
+          <Marker coordinate={{latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.003, longitudeDelta: 0.003}} />
+        </MapView>
+      </View>
+
+      {/* search bar */}
+      <View style={searchBarFocused ? styles.searchBarContainerFocused : styles.searchBarContainer}>
+        <GooglePlacesAutocomplete
+          ref={(instance) => { setSearchBar(instance) }}
+          styles={searchBarFocused ?
+            {textInputContainer: styles.textInputContainerFocused, textInput: styles.textInput, listView: styles.listView} :
+            {textInputContainer: styles.textInputContainer, textInput: styles.textInput, listView: styles.listView}}
+          placeholder='Search'
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            setLocation({latitude: details.geometry.location.lat, longitude: details.geometry.location.lng});
+            setSearchBarFocused(false);
+            searchBar.setAddressText("");
+          }}
+          textInputProps={{
+            onFocus: () => setSearchBarFocused(true),
+          }}
+          query={{key: 'AIzaSyBvfVumttk96MLwUy-oLqaz3OqtGSIAejk', components: 'country:vn',}}
+          debounce={150}
+        />
+      </View>
+      <View style={searchBarFocused ? styles.searchBackBtnFocused : styles.searchBackBtn}>
+        <Icon name='arrow-left' type='font-awesome'
+          onPress={() => {
+            setSearchBarFocused(false);
+            Keyboard.dismiss();
+          }}/>
+      </View>
+
+      <View style={styles.controlPanel}>
+        <Text>Control Panel</Text>
+      </View>
+
     </SafeAreaView>
   );
 };
--- app/main/components/LoginScreen/styles/index.css.js
+++ app/main/components/LoginScreen/styles/index.css.js
@@ -1,8 +1,11 @@
 import {StyleSheet} from 'react-native';
 import Constants from 'expo-constants';
+import { COLORS } from "../../../../const/color"; 
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const styles = StyleSheet.create({
+
+  // copy
   container : {
     flex: 1,
     justifyContent: 'center',
@@ -11,6 +14,23 @@ const styles = StyleSheet.create({
     paddingLeft: wp('10%'),
     paddingRight: wp('10%'),
   },
+  header : {
+    width: "100%",
+    height: hp("34%"),
+    backgroundColor: COLORS.LIGHT_ORANGE,
+    borderBottomLeftRadius: wp("50%"),
+    borderBottomRightRadius: wp("50%"),
+  },
+  avatar : {
+    width: wp("12%"),
+    height: wp("12%"),
+    marginTop: hp("7%"),
+    marginLeft: wp("10%"),
+    borderRadius: wp("6%"),
+    marginBottom: hp("1%"),
+  },
+
+  // gg maps
   searchBarContainer: {
     position: "absolute",
     top: 0,
@@ -71,7 +91,20 @@ const styles = StyleSheet.create({
     left: 10,
     height: 25,
     width: 25
+  },
+
+  // control panel
+  controlPanel:{
+    position: "absolute",
+    top: "auto",
+    bottom: 0,
+    left: wp('5%'),
+    right: wp('5%'),
+    height: 200,
+    borderRadius: 20,
+    backgroundColor: COLORS.STRONG_ORANGE
   }
+
 });
 
 export default styles;
