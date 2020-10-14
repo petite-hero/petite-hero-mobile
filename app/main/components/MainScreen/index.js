d44cabdf0cf1af42e78cd9e1cf5bbd2a1708d3e8
truongvlit <truongvlit@gmail.com>
2020-10-15 02:28:56 +0700

    change navigation

--- app/main/components/MainScreen/index.js
+++ app/main/components/MainScreen/index.js
@@ -1,81 +1,35 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-// import FontAwesome from "react-native-vector-icons/FontAwesome";
-import LoginScreen from "../LoginScreen/index";
-import RegisterScreen from "../RegisterScreen/index";
-import styles from './styles/index.css';
-import ProfileScreen from '../ProfileScreen';
-import WelcomeScreen from '../WelcomeScreen';
-import QuestScreen from '../QuestScreen';
-import TaskScreen from '../TaskScreen';
 import TrackingStatusScreen from '../TrackingStatusScreen';
-import { COLORS } from '../../../const/const';
+import { createStackNavigator } from '@react-navigation/stack';
+import TrackingSettingsScreen from '../TrackingSettingsScreen';
+import TrackingEmergencyScreen from '../TrackingEmergencyScreen';
 
-const Tab = createMaterialTopTabNavigator();
+const Stack = createStackNavigator();
 
 const MainScreen = ({route}) => {
   return(
-    <Tab.Navigator
-      screenOptions = {({route}) => ({
-        tabBarIcon: ({ focused, color }) => {
-          let icon;
-          if (route.name == 'Tracking') {
-            icon = focused
-              ? require('../../../../assets/nav-bar/tracking-active.png')
-              : require('../../../../assets/nav-bar/tracking.png')
-          } else if (route.name == 'Tasks') {
-            icon = focused
-              ? require('../../../../assets/nav-bar/task-active.png')
-              : require('../../../../assets/nav-bar/task.png')
-          } else if (route.name == 'Quests') {
-            icon = focused
-              ? require('../../../../assets/nav-bar/quest-active.png')
-              : require('../../../../assets/nav-bar/quest.png')
-          } else if (route.name == 'Profile') {
-            icon = focused
-              ? require('../../../../assets/nav-bar/profile-active.png')
-              : require('../../../../assets/nav-bar/profile.png')
-          }
-          return (
-            <Image style={styles.tabIcon} source={icon}/>
-          )
-        }
-      })}
-      tabBarOptions={{
-        labelStyle: {
-          fontSize: 10,
-          fontWeight: "bold",
-          textTransform: "none"
-        },
-        style: styles.bottomTab,
-        tabStyle: styles.tabItem,
-        indicatorStyle: styles.indicator,
-        showIcon: true,
-        activeTintColor: COLORS.STRONG_ORANGE,
-        inactiveTintColor: COLORS.GREY,
+    <Stack.Navigator 
+      initialRouteName="TrackingStatus"
+      screenOptions={{
+        headerShown: false
       }}
-      tabBarPosition="bottom"
-    >
-      <Tab.Screen 
-        name="Tracking" 
+      >
+      <Stack.Screen 
+        name="TrackingStatus"
         component={TrackingStatusScreen}
         initialParams={{ authContext: route.params.authContext, localizationContext: route.params.localizationContext }}
       />
-      <Tab.Screen 
-        name="Tasks" 
-        component={TaskScreen}
+      <Stack.Screen
+        name="TrackingSettings"
+        component={TrackingSettingsScreen}
         initialParams={{ authContext: route.params.authContext, localizationContext: route.params.localizationContext }}
       />
-      <Tab.Screen name="Quests" 
-        component={QuestScreen}
+      <Stack.Screen
+        name="TrackingEmergency"
+        component={TrackingEmergencyScreen}
         initialParams={{ authContext: route.params.authContext, localizationContext: route.params.localizationContext }}
       />
-      <Tab.Screen name="Profile" 
-        component={ProfileScreen}
-        initialParams={{ authContext: route.params.authContext, localizationContext: route.params.localizationContext }}
-      />
-    </Tab.Navigator>
+    </Stack.Navigator>
   );
 }
 
--- app/main/components/MainScreen/styles/index.css.js
+++ app/main/components/MainScreen/styles/index.css.js
@@ -3,35 +3,6 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 import { COLORS } from '../../../../const/const';
 
 const styles = StyleSheet.create({
-  bottomTab : {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 20,
-    height: hp('8%'),
-    borderRadius: hp('4%'),
-    marginLeft: wp('5%'),
-    marginRight: wp('5%'),
-    backgroundColor: "#ffffff",
-    justifyContent: "center",
-  },
-  tabItem : {
-    alignSelf: "center",
-  },
-  tabIcon : {
-    width: hp('4%'),
-    height: hp('4%'),
-    borderRadius: hp('2%'),
-    alignSelf: "center",
-  },
-  indicator : {
-    position: "absolute",
-    top: 0,
-    width: wp('10%'),
-    height: 3,
-    marginLeft: wp("6.25%"),
-    backgroundColor: COLORS.STRONG_ORANGE,
-  }
 });
 
 export default styles;
