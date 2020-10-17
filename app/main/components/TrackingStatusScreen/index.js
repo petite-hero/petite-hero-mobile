b62fb75d57c480acf3ae49945b4b5c22f134ea3c
hulk1999 <hungng300899@gmail.com>
2020-10-17 18:48:22 +0700

    update

--- app/main/components/TrackingStatusScreen/index.js
+++ app/main/components/TrackingStatusScreen/index.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import { SafeAreaView, Text, TouchableOpacity, View, Image, Animated } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, View, Image, Animated, Easing } from 'react-native';
 import { Icon } from 'react-native-elements';
 import styles from './styles/index.css';
 import { COLORS, IP, PORT } from '../../../const/const';
@@ -89,67 +89,65 @@ const TrackingStatusScreenContent = ({ navigation }) => {
   const responseListener = useRef();
   [newLoc, setNewLoc] = React.useState(null);
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
 
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+  //   registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
-    // This listener is fired whenever a notification is received while the app is foregrounded
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+  //   // This listener is fired whenever a notification is received while the app is foregrounded
+  //   notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
 
-      setNotification(notification);
+  //     setNotification(notification);
       
-      if (notification.request.content.title === null) {
-        console.log("Update child's location on Tracking screen please!");
-        // insert codes to update child's location here
-        let tmpNewLoc = notification.request.content.data;
-        if (tmpNewLoc.status === "1" && trackingStatus === "NOT SAFE") setTrackingStatus("SAFE");
-        if (tmpNewLoc.status === "0" && trackingStatus === "SAFE") setTrackingStatus("NOT SAFE"); 
-        setNewLoc(notification.request.content.data);
-        console.log(notification.request.content.data);
-        // access location data by notification.request.content.data
-      } else {
-        // insert codes to handle something else here
-      }
-    });
+  //     if (notification.request.content.title === null) {
+  //       console.log("Update child's location on Tracking screen please!");
+  //       // insert codes to update child's location here
+  //       let tmpNewLoc = notification.request.content.data;
+  //       if (tmpNewLoc.status === "1" && trackingStatus === "NOT SAFE") setTrackingStatus("SAFE");
+  //       if (tmpNewLoc.status === "0" && trackingStatus === "SAFE") setTrackingStatus("NOT SAFE"); 
+  //       setNewLoc(notification.request.content.data);
+  //       console.log(notification.request.content.data);
+  //       // access location data by notification.request.content.data
+  //     } else {
+  //       // insert codes to handle something else here
+  //     }
+  //   });
 
-    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => { 
-    //   console.log(response);
-    });
+  //   // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+  //   responseListener.current = Notifications.addNotificationResponseReceivedListener(response => { 
+  //   //   console.log(response);
+  //   });
 
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener);
-      Notifications.removeNotificationSubscription(responseListener);
-    };
-  }, []);
+  //   return () => {
+  //     Notifications.removeNotificationSubscription(notificationListener);
+  //     Notifications.removeNotificationSubscription(responseListener);
+  //   };
+  // }, []);
 
-  async function registerForPushNotificationsAsync() {
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    if (status !== 'granted') {
-      alert('You need to grant permission to receive Notifications!');
-      return;
-    }
-    let token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log("Your device token: ", token);
-    return token;
-  }
+  // async function registerForPushNotificationsAsync() {
+  //   const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  //   if (status !== 'granted') {
+  //     alert('You need to grant permission to receive Notifications!');
+  //     return;
+  //   }
+  //   let token = (await Notifications.getExpoPushTokenAsync()).data;
+  //   console.log("Your device token: ", token);
+  //   return token;
+  // }
 
   // end Tuan
 
 
-  [trackingStatus, setTrackingStatus] = React.useState("SAFE");  // INACTIVE, SAFE, NOT SAFE
+  [trackingStatus, setTrackingStatus] = React.useState("NOT SAFE");  // INACTIVE, SAFE, NOT SAFE
   const STATUS_COLORS = {"INACTIVE": "rgb(140, 140, 140)", "SAFE": "rgb(0, 154, 34)", "NOT SAFE": "red"};
 
   // [elapsedPercent, setElapsedPercent] = React.useState(0);
   const CENTER_RATIO = 0.6;
   const DIAMETER = wp("70%");
-  const DURATION = 1000;
+  const DURATION = 2000;
 
-  const scale1 = React.useRef(new Animated.Value(1)).current;
-  const opac1 = React.useRef(new Animated.Value(1)).current;
+  const timer = React.useRef(new Animated.Value(0)).current;
   React.useEffect(() => {
-    Animated.loop(Animated.timing(scale1, {toValue: 1/CENTER_RATIO, duration: DURATION, useNativeDriver: true})).start();
-    Animated.loop(Animated.timing(opac1, {toValue: 0, duration: DURATION, useNativeDriver: true})).start();
+    Animated.loop(Animated.timing(timer, {toValue: 1000, duration: DURATION, easing: Easing.linear, useNativeDriver: true})).start();
   });
 
   return (
@@ -161,46 +159,52 @@ const TrackingStatusScreenContent = ({ navigation }) => {
         source={require('../../../../assets/kid-avatar.png')}
       />
 
-      {/* <Animated.View style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
-              {width: DIAMETER*CENTER_RATIO, height: DIAMETER*CENTER_RATIO, opacity: fadeAnim}]}/> */}
-
-      <View style={styles.statusContainer}>
-        <Animated.View style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus],
-                       transform: [{scaleX: scale1}, {scaleY: scale1}],
-                       width: DIAMETER*CENTER_RATIO, height: DIAMETER*CENTER_RATIO, opacity: opac1}]}/>
-        {/* {trackingStatus === "INACTIVE" ?
-          [
-            <View key={-1} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
-                  {width: DIAMETER*0.7333, height: DIAMETER*0.7333, opacity: 0.66675}]}/>,
-            <View key={-2} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
-                  {width: DIAMETER*0.8667, height: DIAMETER*0.8667, opacity: 0.33325}]}/>
-          ]
-          :
-          [1, 2, 3].map((el, index) => {
-            let ratio = CENTER_RATIO + ((elapsedPercent+33*index)%100)/100*(1-CENTER_RATIO);
-            return (
-              <Animated.View key={index} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
-                    {width: DIAMETER*ratio, height: DIAMETER*ratio, opacity: 1-(ratio-CENTER_RATIO)/(1-CENTER_RATIO)}]}/>
-            )
-          })
-        }
-        <View style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
-              {width: DIAMETER*CENTER_RATIO, height: DIAMETER*CENTER_RATIO}]}/>
-        <Text style={styles.locationStatus}>{trackingStatus}</Text> */}
-      </View>
-
       <TouchableOpacity style={styles.warningBtn} onPress={() => navigation.navigate("TrackingEmergency")}>
         <Icon name='priority-high' type='material' color='white' size={20}/>
       </TouchableOpacity>
+
+      {/* status container */}
+      <View style={styles.statusContainer}>
+        {trackingStatus === "INACTIVE" ?
+          [
+            <View key={-1} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
+                  {width: DIAMETER*0.733, height: DIAMETER*0.733, opacity: 0.668}]}/>,
+            <View key={-2} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
+                  {width: DIAMETER*0.867, height: DIAMETER*0.867, opacity: 0.333}]}/>
+          ]
+          :
+          [1, 2, 3].map((el, index) => {
+            let timerRatio = Animated.divide(Animated.modulo(Animated.add(timer, 1000*index/3), 1000), 1000);
+            let scale = Animated.add(1, Animated.multiply(timerRatio, 1/CENTER_RATIO-1));
+            let opac = Animated.subtract(1, timerRatio);
+            return (
+              <Animated.View key={index} style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus],
+                             transform: [{scaleX: scale}, {scaleY: scale}],
+                             width: DIAMETER*CENTER_RATIO, height: DIAMETER*CENTER_RATIO, opacity: opac}]}/>
+            )
+          })
+        }
+        <Animated.View style={[styles.statusWave, {backgroundColor: STATUS_COLORS[trackingStatus]},
+                       {width: DIAMETER*CENTER_RATIO, height: DIAMETER*CENTER_RATIO}]}/>
+        <Text style={styles.locationStatus}>{trackingStatus}</Text>
+      </View>
 
       {/* setting buttons */}
       <View style={styles.settingBtnsContainer}>
 
         <TouchableOpacity style={[styles.settingBtnContainer, {backgroundColor: trackingStatus === "INACTIVE" ? "white" : COLORS.STRONG_ORANGE}]}
-                          onPress={() => {
-                            if (trackingStatus === "INACTIVE") setTrackingStatus("SAFE");
-                            else (setTrackingStatus("INACTIVE"));
-                          }}>
+          onPress={() => {
+            if (trackingStatus === "INACTIVE"){
+              setTrackingStatus("SAFE");
+              timer.current = new Animated.Value(0);
+              Animated.loop(Animated.timing(timer, {toValue: 1000, duration: DURATION, easing: Easing.linear, useNativeDriver: true})).start();
+            }
+            else{
+              setTrackingStatus("INACTIVE");
+              timer.stopAnimation();
+            }
+          }}
+        >
           <Icon name="near-me" type="material" size={20} color={trackingStatus === "INACTIVE" ? "rgb(140, 140, 140)" : "white"}/>
         </TouchableOpacity>
         <TouchableOpacity style={styles.settingBtnContainer} onPress={() => navigation.navigate("TrackingSettings")}>
@@ -215,7 +219,6 @@ const TrackingStatusScreenContent = ({ navigation }) => {
 
         <Text style={[styles.txtSettingBtnGuide, {top: 60}]}>Safe Zone for Selected Day</Text>
         <Text style={[styles.txtSettingBtnGuide, {top: 110}]}>Safe Zone for Tomorrow</Text>
-        {/* <Text style={[styles.txtSettingBtnGuide, {top: 110}]}>{newLoc}</Text> */}
 
       </View>
 
